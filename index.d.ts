04998eb6508030db616a07858a9a9e169f06df4d
Dimitri Benin <dimitri.benin@gmail.com>
2019-03-31 23:30:04 +0200

    Refactor TypeScript definition to CommonJS compatible export

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,26 @@
-/**
- * Capitalize every word in a string: `unicorn cake` → `Unicorn Cake`.
- *
- * @param input - The string to titleize.
- */
-export default function titleize(input: string): string;
+declare const titleize: {
+	/**
+	Capitalize every word in a string: `unicorn cake` → `Unicorn Cake`.
+
+	@param input - The string to titleize.
+
+	@example
+	```
+	import titleize = require('titleize');
+
+	titleize('foo bar');
+	//=> 'Foo Bar'
+
+	titleize('foo-bar');
+	//=> 'Foo-Bar'
+	```
+	*/
+	(input: string): string;
+
+	// TODO: Remove this for the next major release, refactor the whole definition to:
+	// declare function titleize(input: string): string;
+	// export = titleize;
+	default: typeof titleize;
+};
+
+export = titleize;
